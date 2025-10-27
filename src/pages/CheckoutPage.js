5d31daa19b6599bd3e596e5da5a9367ba5695a13
kunalrajput04 <kunalsingh67678@gmail.com>
2025-10-27 19:28:15 +0530

    //

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,4 +1,6 @@
+// src/pages/CheckoutPage.js
 import React from "react";
+import { Link } from "react-router-dom";
 import { Header } from "../components/index/Header";
 import Footer from "../components/index/Footer";
 
@@ -6,218 +8,194 @@ export const CheckoutPage = () => {
   return (
     <div>
       <Header />
+
       <main>
-        {/* BREADCRUMB SECTION START  */}
+        {/* BREADCRUMB */}
         <div className="ul-container">
           <div className="ul-breadcrumb">
             <h2 className="ul-breadcrumb-title">Checkout</h2>
-            <div className="ul-breadcrumb-nav">
-              <a href="index.html">
-                <i className="flaticon-home"></i> Home
-              </a>
-              <i className="flaticon-arrow-point-to-right"></i>
-              <span className="current-page">Checkout</span>
-            </div>
+            <nav className="ul-breadcrumb-nav" aria-label="Breadcrumb">
+              <Link to="/">
+                <i className="flaticon-home" /> Home
+              </Link>
+              <i className="flaticon-arrow-point-to-right" aria-hidden="true" />
+              <span className="current-page" aria-current="page">
+                Checkout
+              </span>
+            </nav>
           </div>
         </div>
-        {/* BREADCRUMB SECTION END  */}
 
-        {/* CHEKOUT SECTION START  */}
-        <div className="ul-cart-container">
-          <h3 className="ul-checkout-title">Billing Details</h3>
-          <form action="#" className="ul-checkout-form">
-            <div className="row ul-bs-row row-cols-2 row-cols-xxs-1">
-              {/* left side / checkout form  */}
-              <div className="col">
-                <div className="row row-cols-lg-2 row-cols-1 ul-bs-row">
-                  {/* first name  */}
-                  <div className="form-group">
-                    <label for="firstname">First Name*</label>
-                    <input
-                      type="text"
-                      name="firstname"
-                      id="firstname"
-                      placeholder="Enter Your First Name"
-                    />
-                  </div>
+        {/* DESKTOP GRID LAYOUT */}
+        <div className="ul-container ul-checkout-grid">
+          {/* LEFT: BILLING FORM */}
+          <section className="ul-checkout-left">
+            <h3 className="ul-checkout-title">Billing Details</h3>
 
-                  {/* last name  */}
-                  <div className="form-group">
-                    <label for="lastname">Last Name*</label>
-                    <input
-                      type="text"
-                      name="lastname"
-                      id="lastname"
-                      placeholder="Enter Your First Name"
-                    />
-                  </div>
-
-                  {/* company name  */}
-                  <div className="form-group">
-                    <label for="companyname">Company Name</label>
-                    <input
-                      type="text"
-                      name="companyname"
-                      id="companyname"
-                      placeholder="Enter Your Company Name"
-                    />
-                  </div>
-
-                  {/* country  */}
-                  <div className="form-group ul-checkout-country-wrapper">
-                    <label for="ul-checkout-country">Country*</label>
-                    <select name="country" id="ul-checkout-country">
-                      <option data-placeholder="true">Select Country</option>
-                      <option value="2">United States</option>
-                      <option value="3">United Kingdom</option>
-                      <option value="4">Germany</option>
-                      <option value="5">France</option>
-                      <option value="6">India</option>
-                    </select>
-                  </div>
-
-                  {/* address 1  */}
-                  <div className="form-group">
-                    <label for="address1">Street Address*</label>
-                    <input
-                      type="text"
-                      name="address1"
-                      id="address1"
-                      placeholder="1837 E Homer M Adams Pkwy"
-                    />
-                  </div>
-
-                  {/* address 2  */}
-                  <div className="form-group">
-                    <label for="address2">Address 2*</label>
-                    <input
-                      type="text"
-                      name="address2"
-                      id="address2"
-                      placeholder="1837 E Homer M Adams Pkwy"
-                    />
-                  </div>
-
-                  {/* city  */}
-                  <div className="form-group">
-                    <label for="city">City or Town*</label>
-                    <input
-                      type="text"
-                      name="city"
-                      id="city"
-                      placeholder="Enter Your City or Town"
-                    />
-                  </div>
-
-                  {/* state  */}
-                  <div className="form-group">
-                    <label for="state">State*</label>
-                    <input
-                      type="text"
-                      name="state"
-                      id="state"
-                      placeholder="Enter Your State"
-                    />
-                  </div>
-
-                  {/* postcode  */}
-                  <div className="form-group">
-                    <label for="zipcode">ZIP Code*</label>
-                    <input
-                      type="text"
-                      name="zipcode"
-                      id="zipcode"
-                      placeholder="Enter Your Postcode"
-                    />
-                  </div>
-
-                  {/* phone  */}
-                  <div className="form-group">
-                    <label for="phone">Phone*</label>
-                    <input
-                      type="text"
-                      name="phone"
-                      id="phone"
-                      placeholder="Enter Your Phone Number"
-                    />
-                  </div>
-
-                  {/* email  */}
-                  <div className="form-group col-lg-12">
-                    <label for="email">Email Address*</label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="Enter Your Email"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              {/* right side / different address  */}
-              <div className="col">
+            <form action="#" className="ul-checkout-form" onSubmit={(e) => e.preventDefault()}>
+              <div className="row ul-bs-row row-cols-lg-2 row-cols-1">
+                {/* First Name */}
                 <div className="form-group">
-                  <label for="ul-checkout-different-address-field">
-                    Shift to A Different Address
+                  <label htmlFor="firstname">First Name*</label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    placeholder="Enter Your First Name"
+                    autoComplete="given-name"
+                    required
+                  />
+                </div>
+
+                {/* Last Name */}
+                <div className="form-group">
+                  <label htmlFor="lastname">Last Name*</label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    placeholder="Enter Your Last Name"
+                    autoComplete="family-name"
+                    required
+                  />
+                </div>
+
+                {/* Company */}
+                <div className="form-group">
+                  <label htmlFor="companyname">Company Name</label>
+                  <input
+                    type="text"
+                    name="companyname"
+                    id="companyname"
+                    placeholder="Enter Your Company Name"
+                    autoComplete="organization"
+                  />
+                </div>
+
+                {/* Country */}
+                <div className="form-group ul-checkout-country-wrapper">
+                  <label htmlFor="ul-checkout-country">Country*</label>
+                  <select name="country" id="ul-checkout-country" required>
+                    <option value="" data-placeholder="true">
+                      Select Country
+                    </option>
+                    <option value="US">United States</option>
+                    <option value="UK">United Kingdom</option>
+                    <option value="DE">Germany</option>
+                    <option value="FR">France</option>
+                    <option value="IN">India</option>
+                  </select>
+                </div>
+
+                {/* Address 1 */}
+                <div className="form-group">
+                  <label htmlFor="address1">Street Address*</label>
+                  <input
+                    type="text"
+                    name="address1"
+                    id="address1"
+                    placeholder="1837 E Homer M Adams Pkwy"
+                    autoComplete="address-line1"
+                    required
+                  />
+                </div>
+
+                {/* Address 2 */}
+                <div className="form-group">
+                  <label htmlFor="address2">Address 2</label>
+                  <input
+                    type="text"
+                    name="address2"
+                    id="address2"
+                    placeholder="Apartment, suite, unit, etc. (optional)"
+                    autoComplete="address-line2"
+                  />
+                </div>
+
+                {/* City */}
+                <div className="form-group">
+                  <label htmlFor="city">City or Town*</label>
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    placeholder="Enter Your City or Town"
+                    autoComplete="address-level2"
+                    required
+                  />
+                </div>
+
+                {/* State */}
+                <div className="form-group">
+                  <label htmlFor="state">State*</label>
+                  <input
+                    type="text"
+                    name="state"
+                    id="state"
+                    placeholder="Enter Your State"
+                    autoComplete="address-level1"
+                    required
+                  />
+                </div>
+
+                {/* ZIP */}
+                <div className="form-group">
+                  <label htmlFor="zipcode">ZIP Code*</label>
+                  <input
+                    type="text"
+                    name="zipcode"
+                    id="zipcode"
+                    placeholder="Enter Your Postcode"
+                    autoComplete="postal-code"
+                    required
+                  />
+                </div>
+
+                {/* Phone */}
+                <div className="form-group">
+                  <label htmlFor="phone">Phone*</label>
+                  <input
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    placeholder="Enter Your Phone Number"
+                    autoComplete="tel"
+                    required
+                  />
+                </div>
+
+                {/* Email */}
+                <div className="form-group col-lg-12">
+                  <label htmlFor="email">Email Address*</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter Your Email"
+                    autoComplete="email"
+                    required
+                  />
+                </div>
+
+                {/* Ship to different address (text area moved under the form) */}
+                <div className="form-group col-lg-12">
+                  <label htmlFor="ul-checkout-different-address-field">
+                    Ship to a Different Address
                   </label>
                   <textarea
                     name="different-address"
                     id="ul-checkout-different-address-field"
-                    placeholder="2801 Lafayette Blvd, Norfolk, Vermont 23509, united state"
-                  ></textarea>
-                </div>
-
-                {/* different address checkbox  */}
-                <div className="ul-checkout-payment-methods">
-                  <div className="form-group">
-                    <label for="payment-option-1">
-                      <input
-                        type="radio"
-                        name="payment-methods"
-                        id="payment-option-1"
-                        hidden
-                        checked
-                      />
-                      <span className="ul-radio-wrapper"></span>
-                      <span className="ul-checkout-payment-method">
-                        <span className="title">Direct Bank Transfer</span>
-                        <span className="descr">
-                          Neque porro est qui dolorem ipsum quia quaed inventor
-                          veritatis et quasi architecto beatae vitae dicta sunt
-                          explicabo. Aelltes port lacus quis enim var sed
-                          efficitur
-                        </span>
-                      </span>
-                    </label>
-                  </div>
-
-                  <div className="form-group">
-                    <label for="payment-option-2">
-                      <input
-                        type="radio"
-                        name="payment-methods"
-                        id="payment-option-2"
-                        hidden
-                      />
-                      <span className="ul-radio-wrapper"></span>
-                      <span className="ul-checkout-payment-method">
-                        <span className="title">
-                          Ship To A Different Address?
-                        </span>
-                      </span>
-                    </label>
-                  </div>
-                  <button type="submit" className="ul-checkout-form-btn">
-                    Place Your Order
-                  </button>
+                    placeholder="2801 Lafayette Blvd, Norfolk, Vermont 23509, United States"
+                    rows={3}
+                  />
                 </div>
               </div>
-            </div>
-          </form>
-          {/*  */}
-          {/* bill summary  */}
-          <div className="row ul-bs-row row-cols-2 row-cols-xxs-1">
+            </form>
+          </section>
+
+          {/* RIGHT: ORDER SUMMARY + PAYMENT (sticky) */}
+          <aside className="ul-checkout-right">
+            {/* Order Summary */}
             <div className="ul-checkout-bill-summary">
               <h4 className="ul-checkout-bill-summary-title">Your Order</h4>
 
@@ -248,10 +226,54 @@ export const CheckoutPage = () => {
                 </div>
               </div>
             </div>
-          </div>
+
+            {/* Payment Methods */}
+            <div className="ul-checkout-payment-methods">
+              <div className="form-group">
+                <label htmlFor="payment-option-1" className="ul-payment-option">
+                  <input
+                    type="radio"
+                    name="payment-methods"
+                    id="payment-option-1"
+                    hidden
+                    defaultChecked
+                  />
+                  <span className="ul-radio-wrapper" aria-hidden="true"></span>
+                  <span className="ul-checkout-payment-method">
+                    <span className="title">Direct Bank Transfer</span>
+                    <span className="descr">
+                      Neque porro est qui dolorem ipsum quia quaed inventor
+                      veritatis et quasi architecto beatae vitae dicta sunt
+                      explicabo. Aelltes port lacus quis enim var sed
+                      efficitur.
+                    </span>
+                  </span>
+                </label>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="payment-option-2" className="ul-payment-option">
+                  <input
+                    type="radio"
+                    name="payment-methods"
+                    id="payment-option-2"
+                    hidden
+                  />
+                  <span className="ul-radio-wrapper" aria-hidden="true"></span>
+                  <span className="ul-checkout-payment-method">
+                    <span className="title">Cash on Delivery</span>
+                  </span>
+                </label>
+              </div>
+
+              <button type="submit" className="ul-checkout-form-btn">
+                Place Your Order
+              </button>
+            </div>
+          </aside>
         </div>
-        {/* CHEKOUT SECTION END  */}
       </main>
+
       <Footer />
     </div>
   );
